5650fddd153fbfa878f58be0bdc3bce3f8178cbf
Lew Bloch <Lew Bloch>
2016-10-06 22:34:42 -0700

    Initial project contents: Swapper.

---
+++ src/com/lewscanon/lessons/littlealgorithm/Swapper.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2016, Lewis S. Bloch.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.lewscanon.lessons.littlealgorithm;
+
+/**
+ * SAM for a {@code swap} method.
+ * How to fit algorithm pieces together in real code.
+ */
+public interface Swapper
+{
+    /**
+     * Swap a pair.
+     *
+     * @param pair Pair of {@code int}s to swap.
+     */
+    void swap(Pair pair);
+
+    /**
+     * Pair of integers.
+     * Members are intentionally public and mutable.
+     * Nested class to restrict it to where we require such looseness.
+     */
+    class Pair
+    {
+        /** Left element. */
+        public int left;
+        /** Right element. */
+        public int right;
+
+        /**
+         * Constructor.
+         */
+        public Pair()
+        {
+        }
+
+        /**
+         * Constructor with initial values.
+         *
+         * @param lef int for the left.
+         * @param rig int for the right.
+         */
+        public Pair(int lef, int rig)
+        {
+            this.left = lef;
+            this.right = rig;
+        }
+
+        @Override public boolean equals(Object other)
+        {
+            if (this == other)
+            {
+                return true;
+            }
+            if (! (other instanceof Pair))
+            {
+                return false;
+            }
+            Pair pair = (Pair) other;
+            final boolean isSo = left == pair.left && right == pair.right;
+            return isSo;
+        }
+
+        @Override public int hashCode()
+        {
+            final int hash = 31 * left + 89 * right;
+            return hash;
+        }
+
+        @Override public String toString()
+        {
+            final String ret = "{" + left + ", " + right + "}";
+            return ret;
+        }
+    }
+}
---
+++ test/src/com/lewscanon/lessons/littlealgorithm/swapper/XorSwapperNGTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2016, Lewis S. Bloch.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.lewscanon.lessons.littlealgorithm.swapper;
+
+import java.lang.reflect.Method;
+import java.util.logging.Logger;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.lewscanon.lessons.littlealgorithm.Swapper;
+import com.lewscanon.lessons.littlealgorithm.Swapper.Pair;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Test the {@link XorSwapper}.
+ */
+public class XorSwapperNGTest
+{
+    private static final String DATAP = "provider";
+
+    private final Logger logger = Logger.getLogger(getClass().getSimpleName());
+
+    /**
+     * Test of swap method, of class XorSwapper.
+     *
+     * @param pair Pair to test.
+     */
+    @Test(dataProvider = DATAP)
+    public void testSwap(Pair pair)
+    {
+        Swapper swapper = new XorSwapper();
+
+        Pair swappee = new Pair(pair.left, pair.right);
+        Pair expected = new Pair(pair.right, pair.left);
+
+        swapper.swap(swappee);
+        assertEquals(swappee, expected, "Swap failure");
+    }
+
+    @DataProvider(name = DATAP)
+    Object[][] provideData(Method test)
+    {
+        final Object[][] data = new Object[][]
+        {
+            { new Pair(17, 89) },
+            { new Pair(0, 1) },
+            { new Pair(1, 0) },
+            { new Pair(0, 0) },
+            { new Pair(-1, 1) },
+            { new Pair(Integer.MAX_VALUE / 2 + 1, Integer.MAX_VALUE / 2) },
+            { new Pair(Integer.MIN_VALUE / 2 + 1, Integer.MIN_VALUE / 2) },
+        };
+        return data;
+    }
+}
