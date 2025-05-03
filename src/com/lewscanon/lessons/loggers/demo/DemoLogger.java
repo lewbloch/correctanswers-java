9172e4309ad28e51226964b879b01bb90472a124
lewbloch <kw2kw@lewscanon.com>
2025-05-03 15:23:39 -0700

    DemoLogger static import of LOGSYSTEM.

--- src/com/lewscanon/lessons/loggers/demo/DemoLogger.java
+++ src/com/lewscanon/lessons/loggers/demo/DemoLogger.java
@@ -1,13 +1,13 @@
 package com.lewscanon.lessons.loggers.demo;
 
-import com.lewscanon.lessons.loggers.LogSystem;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import static com.lewscanon.lessons.loggers.LogSystem.LOGSYSTEM;
+
 public class DemoLogger {
     public static void main(String[] args) {
-        Logger logger = Logger.getLogger(LogSystem.LOGSYSTEM.toString());
+        Logger logger = Logger.getLogger(LOGSYSTEM.toString());
         logger.log(Level.WARNING, "Logging with logger(\"{0}\")", logger.getName());
     }
 }
